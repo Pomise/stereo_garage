d710abc106c826e5b1cb20466ff6d1c12b3fe9ea
Pomise <1340823522@qq.com>
2018-05-02 02:03:54 +0800

    解决当前车位数据的bug，优化车位安排算法，增加圆盘车库双向转动，增加去车槽抖动的

---
+++ garage_system_new/Move_Control.cpp
@@ -0,0 +1,291 @@
+#include "Move_Control.h"
+#include "Plan.h"
+#include <Servo.h>
+#include <Arduino.h>
+#include <math.h>
+
+Servo myservo;
+int pos = 0;
+
+/***********************初始化运动控制************************************/
+void Move_Init(){
+  Enable_Init();           //L298N使能
+  Disk_Init();
+  Disk_Pause(4);          //暂停所有的圆盘车库
+  Step_Init();
+  Fixture_Init();
+  Fixture_Relax();        //夹具放松。
+  Servo_Init();
+}
+void Enable_Init(){
+  pinMode(Fixture_Enable,OUTPUT);
+  pinMode(Disk1_Enable,OUTPUT);
+  pinMode(Disk2_Enable,OUTPUT);
+  pinMode(Disk3_Enable,OUTPUT);
+
+  digitalWrite(Fixture_Enable,HIGH);
+  digitalWrite(Disk1_Enable,HIGH);
+  digitalWrite(Disk2_Enable,HIGH);
+  digitalWrite(Disk3_Enable,HIGH);
+}
+void Disk_Init(){
+  pinMode(Disk1_motor_1,OUTPUT);
+  pinMode(Disk1_motor_2,OUTPUT);
+  pinMode(Disk1_limit,INPUT);
+  pinMode(Disk1_limit,LOW);
+
+  pinMode(Disk2_motor_1,OUTPUT);
+  pinMode(Disk2_motor_2,OUTPUT);
+  pinMode(Disk2_limit,INPUT);
+  pinMode(Disk2_limit,LOW);
+
+  pinMode(Disk3_motor_1,OUTPUT);
+  pinMode(Disk3_motor_2,OUTPUT);
+  pinMode(Disk3_limit,INPUT);
+  pinMode(Disk3_limit,LOW);
+}
+
+void Step_Init(){
+  pinMode(STEP_PIN,OUTPUT);
+  pinMode(DIR_PIN,OUTPUT);
+  pinMode(ENABLE_PIN,OUTPUT);
+  pinMode(Step_Limit_Max,INPUT);
+  pinMode(Step_Limit_Second,INPUT);
+  pinMode(Step_Limit_First,INPUT);
+  pinMode(Step_Limit_Min,INPUT);
+
+  digitalWrite(Step_Limit_Min,LOW);
+  digitalWrite(Step_Limit_First,LOW);
+  digitalWrite(Step_Limit_Second,LOW);
+  digitalWrite(Step_Limit_Max,LOW);
+
+  digitalWrite(STEP_PIN,LOW);
+  digitalWrite(ENABLE_PIN,LOW);
+}
+
+void Fixture_Init(){                       //夹具动力元件初始化。
+  pinMode(Fixture_Motor_1,OUTPUT);
+  pinMode(Fixture_Motor_2,OUTPUT);
+  pinMode(Fixture_Front_Limit,INPUT);
+  pinMode(Fixture_Back_Limit,INPUT);
+
+  digitalWrite(Fixture_Front_Limit,LOW);
+  digitalWrite(Fixture_Back_Limit,LOW);
+}
+
+void Servo_Init(){                        //舵机运动初始化。
+  pinMode(Servo_PIN,OUTPUT);
+}
+
+/**********************定义圆盘电机运动***********************************/
+
+
+void Disk_Move(uchar port,uchar Dir){
+  switch (port){
+    case 0:
+      digitalWrite(Disk1_motor_1,Dir);
+      digitalWrite(Disk1_motor_2,!Dir);
+      break;
+    case 1:
+      digitalWrite(Disk2_motor_1,Dir);
+      digitalWrite(Disk2_motor_2,!Dir);
+      break;
+    case 2:
+      digitalWrite(Disk3_motor_1,Dir);
+      digitalWrite(Disk3_motor_2,!Dir);
+      break;
+  }
+}
+
+void Disk_Pause(uchar Garage){
+  switch (Garage){
+    case 0:
+      digitalWrite(Disk1_motor_1,Motor_Pause);
+      digitalWrite(Disk1_motor_2,Motor_Pause);
+      break;
+    case 1:
+      digitalWrite(Disk2_motor_1,Motor_Pause);
+      digitalWrite(Disk2_motor_2,Motor_Pause);
+      break;
+    case 2:
+      digitalWrite(Disk3_motor_1,Motor_Pause);
+      digitalWrite(Disk3_motor_2,Motor_Pause);
+      break;
+    default:
+      digitalWrite(Disk1_motor_1,Motor_Pause);
+      digitalWrite(Disk1_motor_2,Motor_Pause);
+      digitalWrite(Disk2_motor_1,Motor_Pause);
+      digitalWrite(Disk2_motor_2,Motor_Pause);
+      digitalWrite(Disk3_motor_1,Motor_Pause);
+      digitalWrite(Disk3_motor_2,Motor_Pause);
+      break;
+  }
+  
+}
+
+uchar Read_Disk_Limit(uchar Garage){
+  switch (Garage){
+    case 0:
+      return digitalRead(Disk1_limit);
+    case 1:
+      return digitalRead(Disk2_limit);
+    case 2:
+      return digitalRead(Disk3_limit);
+    case 3:
+      return digitalRead(Disk1_limit);
+  }
+  return 0;
+}
+/*********************************步进电机运动***************************************/
+void Move_Step(){
+  digitalWrite(STEP_PIN,LOW);;
+  delayMicroseconds(5);
+  digitalWrite(STEP_PIN,HIGH);
+}
+
+bool Can_Up(uchar Garage){
+  switch (Garage){
+    case 0:
+      if(digitalRead(Step_Limit_First) == Limit_Invert_Mask)
+        return false;
+      break;
+    case 1:
+      if(digitalRead(Step_Limit_Second)== Limit_Invert_Mask)
+        return false;
+        break;
+    case 2:
+      if(digitalRead(Step_Limit_Max) == Limit_Invert_Mask)
+        return false;
+      break;
+  }
+  return true;
+}
+
+bool Can_Down(){
+  if(digitalRead(Step_Limit_Min) == Limit_Invert_Mask)
+    return false;
+  return true;
+}
+
+/********************************平台运动********************************************/
+void Platform_Ratate(uchar port){
+  
+}
+
+void Platform_Rest(uchar port){
+  
+}
+/*********************************夹具运动控制***************************************/
+void Fixture_Clamp(){
+  myservo.attach(Servo_PIN);
+  for(;pos < Servo_Clamp_angle;pos++){
+    myservo.write(pos);
+    delay(10);
+  }
+  delay(1000);
+}
+
+void Fixture_Relax(){
+  myservo.attach(Servo_PIN);
+  for(;pos> Servo_Relax_angle;pos--){
+    myservo.write(pos);
+  }
+  delay(1000);
+}
+
+void Fixture_Pause(){
+  digitalWrite(Fixture_Motor_1,Motor_Pause);
+  digitalWrite(Fixture_Motor_2,Motor_Pause);
+}
+
+void Fixture_Front(){
+  digitalWrite(Fixture_Motor_1,HIGH);
+  digitalWrite(Fixture_Motor_2,LOW);
+  while(digitalRead(Fixture_Front_Limit) == !Limit_Invert_Mask){
+    delay(10);
+  }
+  Fixture_Pause();
+  delay(1000);
+}
+
+void Fixture_Back(){
+  digitalWrite(Fixture_Motor_1,!Fixture_DIR);
+  digitalWrite(Fixture_Motor_2,Fixture_DIR);
+  while(digitalRead(Fixture_Back_Limit) == !Limit_Invert_Mask){
+    delay(10);
+  }
+  Fixture_Pause();
+  delay(1000);
+}
+/********************************整体运动****************************************/
+void Move_Up(uchar port){
+  uchar Garage = port/Garage_Volume;
+  char Limit_Need = port-Judge_Current(Garage);    //理论上需要限位正反变换一套的次数
+  // if(Limit_Need < 0)
+  //   Limit_Need = Limit_Need + Garage_Volume;
+  uchar Limit_Num = 0;
+  uchar Limit_Flag ；                     //使用前定义。
+  bool  Step_OK = false;
+  bool  Ratate_OK = false;
+
+  if(Limit_Need){                          //如果需要旋转车库圆盘.HIGH定义是正转（逆时针旋转）
+    if(Limit_Need > 0){                                            //需要正向旋转。
+      Limit_Num = Limit_Need*2;                                     //限位变换的次数。
+      if(Read_Disk_Limit(Garage) == !Disk_Limit_Mask) {             //表示因为抖动车槽移位或者上次运动是反转当前车槽不在限位位置
+        Limit_Num++;                                          //限位变换次数加一
+      }
+      Disk_Move(Garage，1);
+      Limit_Flag = Read_Disk_Limit(Garage);
+      for(uchar i=Limit_Num;i > 0;i--){
+        while(Read_Disk_Limit(Garage) == !Limit_Flag){       //知道限位状态发生变换
+          delay(10);
+        }
+        Limit_Flag = !Limit_Flag;
+      }
+    }                                                         //完成正转运动
+
+    if(Limit_Need < 0){                                       //需要逆向旋转
+      Limit_Num = abs(Limit_Need)*2;                           //需要限位变换的次数
+      if(Read_Disk_Limit(Garage) == Disk_Limit_Mask){        //因为抖动或者上次是正转
+        Limit_Num--;                                         //限位变换次数减一
+      }
+      Disk_Move(Garage,0);
+      Limit_Flag = Read_Disk_Limit(Garage);
+      for(uchar i=Limit_Num;i > 0;i--){
+        while(Read_Disk_Limit(Garage) == !Limit_Flag){
+          delay(10);
+        }
+        Limit_Flag = !Limit_Flag;
+      }
+    }
+
+  }
+  Disk_Pause(Garage);                                //停止圆盘车库旋转。
+  Ratate_OK = true;
+
+  digitalWrite(DIR_PIN,DIR_DIREC);         //步进电机方向控制。
+  while(1){
+    if(!Step_OK){
+      if(Can_Up(Garage)){
+        Move_Step();
+      }
+      else
+        Step_OK = true;
+    }
+    if(Step_OK)
+      break;
+    delayMicroseconds(Speed);
+   }
+}
+
+void Move_Down(){
+  digitalWrite(DIR_PIN,!DIR_DIREC);
+  while(1){
+    if(Can_Down())
+      Move_Step();
+    else
+      break;
+    delayMicroseconds(Speed);
+  }
+
+}
---
+++ garage_system_new/Plan.cpp
@@ -0,0 +1,100 @@
+#include "Plan.h"
+#include "Data_WR.h"
+#include "Arduino.h"
+#include <math.h>
+
+/******************************定义变量*********************************/
+uchar * Park_Table ;
+uchar Current_Port[Park_Num];
+/********************************************************************/
+
+void Plan_Init(){
+  Park_Table = Read_Table();
+}
+
+void Record_Car_Data(uchar port,uchar *UID){
+  Write_Data(port,UID);
+}
+
+void Record_Garage_Current(uchar Port){
+  Write_Current(Port);
+}
+
+uchar Empty_Num(){                         //判断空车位数量
+  uchar n=0;
+  for(uchar i=0;i<Car_Num;i++){
+    if ((Park_Table[i] & 0x80) == 0)
+      n++;
+  }
+  return n;
+}
+
+void Clear_Port(uchar port){
+  Write_Clear(port);
+}
+
+bool Judge_Can(){                       //判断能否存车
+  uchar i;
+  for(i=0;i<Car_Num;i++){
+    if((Park_Table[i]&0x80)== 0){
+      return true;
+    }
+  }
+  Serial.print(i);
+  return false;
+}
+
+uchar Judge_Port(){
+  uchar i,Garage;
+  ucahr Current;
+  uchar n = 0;
+  ucahr temp = 0;           //用于排序
+  for(i=0;i<Car_Num;i++){
+    if((Park_Table[i]&0x80) == 0)
+      break;
+  }
+  Garage = i/Garage_Volume;
+  Current = Judge_Current(Garage);
+  if((Park_Table[Current]&0x80) == 0)
+    return Park_Table[Current]&0x7F;
+  for(i=0,i<Garage_Volume/2;i++){
+    temp = Current + i;
+    if(temp >= Garage*10+Garage_Volume)
+      temp = temp -Garage_Volume;
+    if((Park_Table[temp]&0x80) == 0)
+      return Park_Table[temp]&0x7F;
+
+    temp = Current - i;
+    if(i < Garage*10)
+      temp = temp + Garage_Volume;
+    if((Park_Table[temp]&0x80) == 0)
+      return Park_Table[temp]&0x7F;
+  }
+  return Park_Table[temp]&0x7F;        //用不到
+}
+
+uchar Judge_Current(uchar Garage){
+  return Read_Current(Garage);
+}
+
+/**判断是存车还是取车（返回一个数字<Car_Num则是取车，数字为车位号）*******************************/ 
+
+uchar Juge_UID(uchar * ID){
+  uchar *Temp_ID;
+  uchar i;
+  for(i=0;i<Car_Num;i++){
+    if((Park_Table[i]&0x80) == 0x80){
+      Temp_ID = Read_UID(i);
+      for(uchar j=0;j< UID_Size;j++){
+        if(Temp_ID[j] == ID[j]){
+          if (j == UID_Size-1){
+            return (Park_Table[i]&0x7F);
+          }
+        }
+        else
+          break;
+      }
+    }
+  }
+  return i;                      //i此时的值等于CarNum.
+}
